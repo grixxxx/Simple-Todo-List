77d4fd98cdc62407bde034f894e76289392ba776
Delmer Ballesteros <delmerjohnb@gmail.com>
2022-11-26 23:18:54 +0800

    add responsive

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -1,39 +1,5 @@
-// const inputEl = document.querySelector("input");
 const btnEl = document.querySelector("button");
 const list = document.querySelector("ul");
-// inputEl.value = "";
-// btnEl.addEventListener("click", () => {
-//   if (!inputEl.value) {
-//     alert("error");
-//   } else {
-//     const listItem = document.createElement("li");
-//     const listItemText = document.createElement("p");
-//     const btnContainer = document.createElement("div");
-//     listItemText.innerText = inputEl.value;
-//     listItemText.setAttribute("contenteditable", "true");
-//     listItemText.classList.add("listItemText");
-//     listItem.appendChild(listItemText);
-//     listItem.classList.add("listItem");
-//     list.appendChild(listItem);
-//     listItem.appendChild(btnContainer);
-//     btnContainer.classList.add("btnContainer");
-//     inputEl.value = "";
-//     const doneBtn = document.createElement("button");
-//     doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-//     btnContainer.appendChild(doneBtn);
-//     doneBtn.addEventListener("click", () => {
-//       listItemText.style.cssText =
-//         "text-decoration:line-through;   color: gray";
-//       listItemText.removeAttribute("contenteditable");
-//     });
-//     const delBtn = document.createElement("button");
-//     delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-//     btnContainer.appendChild(delBtn);
-//     delBtn.addEventListener("click", () => {
-//       list.removeChild(listItem);
-//     });
-//   }
-// });
 const modal = document.querySelector(".createModal");
 const createBtn = document.querySelector(".createBtn");
 const modalBody = document.querySelector(".modalBody");
@@ -47,40 +13,50 @@ closeBtn.addEventListener("click", ()=>{
     modalBody.value = "";
     modalTitle.value = "";
 });
+const listItem = document.createElement("li");
 createBtn.addEventListener("click", ()=>{
-    const listItem = document.createElement("li");
-    listItem.classList.add("listItem");
-    const listItemTitle = document.createElement("h1");
-    const listItemText = document.createElement("p");
-    const listItemContainer = document.createElement("div");
-    listItemContainer.classList.add("listItemContainer");
-    listItemTitle.classList.add("listItemContainer-title");
-    listItemText.classList.add("listItemContainer-message");
-    listItemContainer.appendChild(listItemTitle);
-    listItemContainer.appendChild(listItemText);
-    listItem.appendChild(listItemContainer);
-    listItemTitle.textContent = modalTitle.value;
-    listItemText.textContent = modalBody.value;
-    list.appendChild(listItem);
-    const buttonContainer = document.createElement("div");
-    buttonContainer.classList.add("listItemContainer-button");
-    listItem.appendChild(buttonContainer);
-    const doneBtn = document.createElement("button");
-    const delBtn = document.createElement("button");
-    doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-    delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-    buttonContainer.appendChild(doneBtn);
-    buttonContainer.appendChild(delBtn);
-    modal.close();
-    modalBody.value = "";
-    modalTitle.value = "";
-    doneBtn.addEventListener("click", ()=>{
-        listItemTitle.style.cssText = "text-decoration:line-through;   color: gray";
-        listItemText.style.cssText = "text-decoration:line-through;   color: gray";
-    });
-    delBtn.addEventListener("click", ()=>{
-        list.removeChild(listItem);
-    });
+    if (!modalTitle.value) alert("Error");
+    else {
+        const listItem = document.createElement("li");
+        listItem.classList.add("listItem");
+        const listItemTitle = document.createElement("h1");
+        listItemTitle.classList.add("listItemContainer-title");
+        const listItemText = document.createElement("p");
+        listItemText.classList.add("listItemContainer-message");
+        listItemTitle.textContent = modalTitle.value;
+        listItemText.textContent = modalBody.value;
+        const listItemContainer = document.createElement("div");
+        listItemContainer.classList.add("listItemContainer");
+        const buttonContainer = document.createElement("div");
+        buttonContainer.classList.add("listItemContainer-button");
+        const doneBtn = document.createElement("button");
+        const delBtn = document.createElement("button");
+        doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+        delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+        list.appendChild(listItem);
+        listItemContainer.appendChild(listItemTitle);
+        listItemContainer.appendChild(listItemText);
+        listItem.appendChild(listItemContainer);
+        listItem.appendChild(buttonContainer);
+        buttonContainer.appendChild(doneBtn);
+        buttonContainer.appendChild(delBtn);
+        list.removeChild(emptyMessage);
+        modal.close();
+        modalBody.value = "";
+        modalTitle.value = "";
+        doneBtn.addEventListener("click", ()=>{
+            listItemTitle.style.cssText = "text-decoration:line-through;   color: gray";
+            listItemText.style.cssText = "text-decoration:line-through;   color: gray";
+        });
+        delBtn.addEventListener("click", ()=>{
+            list.removeChild(listItem);
+        });
+    }
 });
+const emptyMessage = document.createElement("p");
+emptyMessage.classList.add("emptyMessage");
+emptyMessage.textContent = "looks like you don't have plans for today....";
+emptyMessage.style.cssText = "color: black; opacity: 0.2; font-size: 0.8rem";
+if (!list.contains(listItem)) list.appendChild(emptyMessage);
 
 //# sourceMappingURL=index.de5c0784.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,5 @@
-// const inputEl = document.querySelector("input");
 const btnEl = document.querySelector("button");
 const list = document.querySelector("ul");
-
-// inputEl.value = "";
-
-// btnEl.addEventListener("click", () => {
-//   if (!inputEl.value) {
-//     alert("error");
-//   } else {
-//     const listItem = document.createElement("li");
-//     const listItemText = document.createElement("p");
-//     const btnContainer = document.createElement("div");
-//     listItemText.innerText = inputEl.value;
-//     listItemText.setAttribute("contenteditable", "true");
-//     listItemText.classList.add("listItemText");
-//     listItem.appendChild(listItemText);
-//     listItem.classList.add("listItem");
-//     list.appendChild(listItem);
-//     listItem.appendChild(btnContainer);
-//     btnContainer.classList.add("btnContainer");
-
-//     inputEl.value = "";
-
-//     const doneBtn = document.createElement("button");
-//     doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-//     btnContainer.appendChild(doneBtn);
-
-//     doneBtn.addEventListener("click", () => {
-//       listItemText.style.cssText =
-//         "text-decoration:line-through;   color: gray";
-//       listItemText.removeAttribute("contenteditable");
-//     });
-
-//     const delBtn = document.createElement("button");
-//     delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
-//     btnContainer.appendChild(delBtn);
-
-//     delBtn.addEventListener("click", () => {
-//       list.removeChild(listItem);
-//     });
-//   }
-// });
 
 const modal = document.querySelector(".createModal");
 const createBtn = document.querySelector(".createBtn");
@@ -59,50 +18,71 @@ closeBtn.addEventListener("click", () => {
   modalTitle.value = "";
 });
 
+const listItem = document.createElement("li");
+
 createBtn.addEventListener("click", () => {
-  const listItem = document.createElement("li");
-  listItem.classList.add("listItem");
-  const listItemTitle = document.createElement("h1");
-  const listItemText = document.createElement("p");
-  const listItemContainer = document.createElement("div");
+  if (!modalTitle.value) {
+    alert("Error");
+  } else {
+    const listItem = document.createElement("li");
+    listItem.classList.add("listItem");
+    const listItemTitle = document.createElement("h1");
+    listItemTitle.classList.add("listItemContainer-title");
+    const listItemText = document.createElement("p");
+    listItemText.classList.add("listItemContainer-message");
+    listItemTitle.textContent = modalTitle.value;
 
-  listItemContainer.classList.add("listItemContainer");
-  listItemTitle.classList.add("listItemContainer-title");
-  listItemText.classList.add("listItemContainer-message");
-  listItemContainer.appendChild(listItemTitle);
-  listItemContainer.appendChild(listItemText);
-  listItem.appendChild(listItemContainer);
+    listItemText.textContent = modalBody.value;
 
-  listItemTitle.textContent = modalTitle.value;
+    const listItemContainer = document.createElement("div");
+    listItemContainer.classList.add("listItemContainer");
 
-  listItemText.textContent = modalBody.value;
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("listItemContainer-button");
 
-  list.appendChild(listItem);
+    const doneBtn = document.createElement("button");
+    const delBtn = document.createElement("button");
 
-  const buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("listItemContainer-button");
-  listItem.appendChild(buttonContainer);
+    doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
+    delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
 
-  const doneBtn = document.createElement("button");
-  const delBtn = document.createElement("button");
+    list.appendChild(listItem);
 
-  doneBtn.innerHTML = `<i class="fa-solid fa-circle-check"></i>`;
-  delBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    listItemContainer.appendChild(listItemTitle);
+    listItemContainer.appendChild(listItemText);
+    listItem.appendChild(listItemContainer);
 
-  buttonContainer.appendChild(doneBtn);
-  buttonContainer.appendChild(delBtn);
+    listItem.appendChild(buttonContainer);
+    buttonContainer.appendChild(doneBtn);
+    buttonContainer.appendChild(delBtn);
 
-  modal.close();
+    list.removeChild(emptyMessage);
 
-  modalBody.value = "";
-  modalTitle.value = "";
+    modal.close();
 
-  doneBtn.addEventListener("click", () => {
-    listItemTitle.style.cssText = "text-decoration:line-through;   color: gray";
-    listItemText.style.cssText = "text-decoration:line-through;   color: gray";
-  });
+    modalBody.value = "";
+    modalTitle.value = "";
 
-  delBtn.addEventListener("click", () => {
-    list.removeChild(listItem);
-  });
+    doneBtn.addEventListener("click", () => {
+      listItemTitle.style.cssText =
+        "text-decoration:line-through;   color: gray";
+      listItemText.style.cssText =
+        "text-decoration:line-through;   color: gray";
+    });
+
+    delBtn.addEventListener("click", () => {
+      list.removeChild(listItem);
+    });
+  }
 });
+
+const emptyMessage = document.createElement("p");
+
+emptyMessage.classList.add("emptyMessage");
+
+emptyMessage.textContent = "looks like you don't have plans for today....";
+emptyMessage.style.cssText = "color: black; opacity: 0.2; font-size: 0.8rem";
+
+if (!list.contains(listItem)) {
+  list.appendChild(emptyMessage);
+}
